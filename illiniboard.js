edf53fffb555b2463b2b3f71eabc73b88eaa4734
Chris Burke <burke1791@gmail.com>
2019-11-25 02:24:58 -0600

    added functionality for populating the DOM

--- illiniboard.js
+++ illiniboard.js
@@ -1,27 +1,80 @@
 let articles = [];
 
-chrome.storage.sync.get(null, items => {
-  for (link in items) {
-    let date = new Date(items[link].pubDate);
-    items[link].pubDate = date;
-    items[link].url = link;
-    articles.push(items[link]);
+updateRecentArticlesNode();
+updateFreeArticlesNode();
+
+function updateRecentArticlesNode() {
+  let recentArticles = document.getElementById('articleList');
+
+  emptyDOMNode(recentArticles);
+
+  let h3 = document.createElement('h3');
+  h3.textContent = 'Latest Unread Posts';
+  
+  recentArticles.appendChild(h3);
+  recentArticles.appendChild(document.createElement('hr'));
+
+  articles = [];
+  
+  chrome.storage.sync.get(null, items => {
+    for (link in items) {
+      if (items[link].pubDate != null) {
+        let date = new Date(items[link].pubDate);
+        items[link].pubDate = date;
+        items[link].url = link;
+        articles.push(items[link]);
+      }
+    }
+  
+    // reorder articles in descending post order
+    articles.sort((a, b) => {return b.pubDate - a.pubDate});
+    addArticlesToDOM();
+  });
+}
+
+function updateFreeArticlesNode() {
+  let freeArticles = document.getElementById('freeArticles');
+
+  emptyDOMNode(freeArticles);
+
+  chrome.storage.sync.get(['lastView', 'freeCount'], items => {
+    // if we don't have a lastView timestamp then the freeCount is worthless because it might be a new month
+    if (items['lastView']) {
+      let lastViewMonth = new Date(items['lastView']).getMonth();
+      let currentMonth = new Date().getMonth();
+
+      if (lastViewMonth == currentMonth) {
+        generateFreeArticlesHTML(items['freeCount']);
+      } else {
+        generateFreeArticlesHTML(6);
+      }
+    } else {
+      generateFreeArticlesHTML(null);
+    }
+  });
+}
+
+const addArticlesToDOM = () => {
+  let count = 0;
+  let newArticles = false;
+
+  for (var article of articles) {
+    if (count <= 2 && !article.viewed) {
+      newArticles = true;
+
+      let articleHTML = generateArticleHTML(article);
+      insertArticleRow(articleHTML);
+
+      count++;
+    }
   }
 
-  // reorder articles in descending post order
-  articles.sort((a, b) => {return b.pubDate - a.pubDate});
-
-  addArticlesToDOM();
-});
-
-let addArticlesToDOM = () => {
-  for (var i = 0; i <= 2; i++) {
-    let articleHTML = generateArticleHTML(articles[i]);
-    insertArticleRow(articleHTML);
+  if (!newArticles) {
+    insertNoNewArticlesRow();
   }
 }
 
-let generateArticleHTML = (articleObj) => {
+const generateArticleHTML = (articleObj) => {
   let div = document.createElement('div');
   div.setAttribute('class', 'article');
 
@@ -49,7 +102,7 @@ let generateArticleHTML = (articleObj) => {
   let pubDate = document.createElement('div');
   pubDate.setAttribute('class', 'pubdate');
   let timestamp = document.createElement('span');
-  timestamp.textContent = articleObj.pubDate;
+  timestamp.textContent = articleObj.pubDate.toLocaleString();
 
   pubDate.appendChild(timestamp);
   left.appendChild(pubDate);
@@ -71,7 +124,27 @@ let generateArticleHTML = (articleObj) => {
   return div;
 }
 
-let insertArticleRow = (articleHTML) => {
+const generateFreeArticlesHTML = (freeCount) => {
+  let freeArticlesNode = document.getElementById('freeArticles');
+
+  if (freeCount != null) {
+    let h3 = document.createElement('h3');
+    h3.textContent = 'Free Stories Remaining: ';
+
+    let span = document.createElement('span');
+    span.textContent = freeCount;
+
+    h3.appendChild(span);
+    freeArticlesNode.appendChild(h3);
+  } else {
+    let h3 = document.createElement('h3');
+    h3.textContent = 'Your free stories will update after your next visit';
+
+    freeArticlesNode.appendChild(h3);
+  }
+}
+
+const insertArticleRow = (articleHTML) => {
   let recentArticles = document.getElementById('articleList');
   let hr = document.createElement('hr');
 
@@ -79,6 +152,35 @@ let insertArticleRow = (articleHTML) => {
   recentArticles.appendChild(hr);
 }
 
-let notInterested = (e) => {
-  console.log(e.target.getAttribute('data-article'));
+const insertNoNewArticlesRow = () => {
+  let recentArticles = document.getElementById('articleList');
+
+  emptyDOMNode(recentArticles);
+
+  recentArticles.appendChild(document.createElement('hr'));
+
+  let p = document.createElement('p');
+  p.textContent = 'No unread articles since your last visit';
+
+  recentArticles.appendChild(p);
+  recentArticles.appendChild(document.createElement('hr'));
+}
+
+function emptyDOMNode(node) {
+  while (node.firstChild) {
+    node.removeChild(node.firstChild);
+  }
+}
+
+const notInterested = (e) => {
+  let link = e.target.getAttribute('data-article');
+
+  chrome.storage.sync.get(link, article => {
+    let keys = Object.keys(article);
+    article[keys[0]].viewed = true;
+
+    chrome.storage.sync.set(article, () => {
+      updateRecentArticlesNode();
+    });
+  });
 }
