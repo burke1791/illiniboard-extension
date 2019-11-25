3fab7435d4d31f1863c6283887cbbbbc76aaa91b
Chris Burke <burke1791@gmail.com>
2019-11-25 02:25:38 -0600

    added functions to update article and view status in chrome storage

---
+++ illiniboardContent.js
@@ -0,0 +1,39 @@
+updateFreeArticleCount();
+updateArticleReadStatus();
+
+// in case some resources are slow to load, this should catch them
+document.onreadystatechange = function () {
+  if (document.readyState === 'complete') {
+    updateFreeArticleCount();
+  }
+}
+
+function updateFreeArticleCount() {
+  let counter = document.getElementsByClassName('tpm-numeral');
+
+  if (counter.length == 1) {
+    let freeCount = Number(counter.item(0).textContent);
+    let now = new Date().toISOString();
+
+    chrome.storage.sync.set({
+      'freeCount': freeCount,
+      'lastView': now
+    });
+  }
+}
+
+function updateArticleReadStatus() {
+  let url = window.location.href;
+
+  url = removeTrailingSlash(url);
+  
+  chrome.storage.sync.get(url, article => {
+    let keys = Object.keys(article);
+    article[keys[0]].viewed = true;
+    chrome.storage.sync.set(article);
+  });
+}
+
+function removeTrailingSlash(url) {
+  return url.replace(/\/+$/, "");
+}
