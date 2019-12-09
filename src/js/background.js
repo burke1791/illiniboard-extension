6c53b175e982588d6d6783e7d641ee24210285e2
Chris Burke <burke1791@gmail.com>
2019-12-09 02:02:32 -0600

    added logic to update the badge on each page refresh

--- src/js/background.js
+++ src/js/background.js
@@ -30,6 +30,9 @@ function requestIlliniboardRSS() {
 
       req.open('GET', ibRSS + ((/\?/).test(ibRSS) ? "&" : "?") + (new Date()).getTime());
       req.send();
+    } else {
+      // update badge in case article read booleans unexpectedly changed
+      updateBadge();
     }
   });
 }
@@ -104,11 +107,11 @@ function updateBadge() {
 
     getUnreadCount(lastView).then(unreadCount => {
       if (unreadCount > 9) {
-        chrome.browserAction.setBadgeText({text: "9+"});
+        chrome.browserAction.setBadgeText({text: '9+'});
       } else if (unreadCount > 0) {
         chrome.browserAction.setBadgeText({text: String(unreadCount)});
       } else {
-        chrome.browserAction.setBadgeText({text: ""});
+        chrome.browserAction.setBadgeText({text: ''});
       }
     });
   });
