059e07febda5c7e0ac71201a38b56830254d5d4e
Chris Burke <burke1791@gmail.com>
2020-06-17 23:17:22 -0500

    refactoring

--- src/utilities.js
+++ src/utilities.js
@@ -1,21 +1,27 @@
 
-const getUnreadArticleCount = (articles, lastViewDate) => {
+export function getUnreadArticleCount(articles, lastViewDate) {
   let unreadCount = 0;
 
   for (var key in articles) {
     let article = articles[key];
 
-    let articlePubDate = article.pubDate != null ? new Date(article.pubDate) : false;
+    let articlePubDate = getArticlePubDate(article);
 
-    // increment the unread count if the current article hasn't been viewed
-    if (articlePubDate && !article.viewed && articlePubDate > lastViewDate) {
-      unreadCount ++;
-    }
+    unreadCount += computeUnreadCount(article, articlePubDate, lastViewDate);
   }
 
   return unreadCount;
 }
 
-export {
-  getUnreadArticleCount
-};
+function getArticlePubDate(article) {
+  return article.pubDate != null ? new Date(article.pubDate) : false
+}
+
+// increment the unread count if the current article hasn't been viewed
+function computeUnreadCount(article, articlePubDate, lastViewDate) {
+  if (articlePubDate && !article.viewed && articlePubDate > lastViewDate) {
+    return 1;
+  }
+
+  return 0;
+}
